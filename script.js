1f57b25827f957d6ff1cbb19ada44a6b72fceb47
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-25 23:56:44 +0500

    mini assignment

---
+++ script.js
@@ -0,0 +1,17 @@
+let matrix1 = [
+      [1, 2, 3],
+      [5, 6, 7],
+      [6, 8, 9],
+];
+let result = []
+for (let i = 0; i < matrix1.length; i++) {
+      result.push([])
+      for (let j = 0; j < matrix1.length; j++) {
+
+            let sum = matrix1[i][j] * matrix1[i][j];
+            result[i].push(sum);
+            console.log(result);
+
+      }
+
+}
